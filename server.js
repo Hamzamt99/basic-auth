3132cbf549d1cfb354cdf775220c5537222e417e
Hamzamt99 <tamarihamza4@gmail.com>
2023-06-24 10:42:53 +0000

    git the package

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+const app = express();
+
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json())
+
+app.listen(3000,()=> console.log('running on port 3000'))
